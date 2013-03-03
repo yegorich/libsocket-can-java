639d518e67d66ffc6c6921e591bc5a8e19cc94b3
Yegor Yefremov <yegorslists@googlemail.com>
2013-03-03 22:04:50 +0100

    introduce proper c++0x support
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,8 +3,7 @@ LOCAL_PATH := $(call my-dir)
 PRIVATE_LOCAL_CFLAGS := -O2 -g -W -Wall		\
 			-DSO_RXQ_OVFL=40	\
 			-DPF_CAN=29		\
-			-DAF_CAN=PF_CAN		\
-			-std=c++0x -std=gnu++0x
+			-DAF_CAN=PF_CAN
 
 include $(CLEAR_VARS)
 
@@ -12,6 +11,7 @@ LOCAL_SRC_FILES := cansocket.cpp
 LOCAL_MODULE := cansocket
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include/
 LOCAL_CFLAGS := $(PRIVATE_LOCAL_CFLAGS)
+LOCAL_CPPFLAGS := -std=c++0x
 
 LOCAL_MODULE_TAGS := optional
 
